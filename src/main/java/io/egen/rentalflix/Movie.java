1f97f24b41d154e9ff7b09fb83c1c735bf031eff
Vatsal Kesarwani <vatsalkesarwani@gmail.com>
2016-02-14 19:01:52 -0600

    feat(Movie):added fields and their getter's & setter's to Movie class, implemented IFlix interface in MovieService class

--- src/main/java/io/egen/rentalflix/Movie.java
+++ src/main/java/io/egen/rentalflix/Movie.java
@@ -6,4 +6,51 @@ package io.egen.rentalflix;
  */
 public class Movie {
 	//POJO IMPLEMENTATION GOES HERE
+	
+	//Movie class private fields
+	private int movieId;
+	private String movieTitle;
+	private String movieYear;
+	private String movieLanguage;
+	private String movieRenter;
+	
+	Movie() {
+		movieRenter = "NOT RENTED";
+	}
+	
+	//Getters and setters for private fields
+	public int getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(int movieId) {
+		this.movieId = movieId;
+	}
+	
+	public String getMovieTitle() {
+		return movieTitle;
+	}
+	public void setMovieTitle(String movieTitle) {
+		this.movieTitle = movieTitle;
+	}
+	
+	public String getMovieYear() {
+		return movieYear;
+	}
+	public void setMovieYear(String movieYear) {
+		this.movieYear = movieYear;
+	}
+	
+	public String getMovieLanguage() {
+		return movieLanguage;
+	}
+	public void setMovieLanguage(String movieLanguage) {
+		this.movieLanguage = movieLanguage;
+	}
+	
+	public String getMovieRenter() {
+		return movieRenter;
+	}
+	public void setMovieRenter(String movieRenter) {
+		this.movieRenter = movieRenter;
+	}
 }
--- src/main/java/io/egen/rentalflix/MovieService.java
+++ src/main/java/io/egen/rentalflix/MovieService.java
@@ -1,9 +1,73 @@
 package io.egen.rentalflix;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Service implementing IFlix interface
  * You can use any Java collection type to store movies
  */
 public class MovieService implements IFlix {
-
+	
+	List<Movie> movieList = new ArrayList<Movie>();
+	
+	public List<Movie> findAll () {
+		return movieList;
+	}
+	
+	public List<Movie> findByName (String name) {
+		List<Movie> movieSearchList = new ArrayList<Movie>();
+		for(Movie movie : movieList) {
+			if(movie.getMovieTitle().toLowerCase().contains(name.toLowerCase())) {
+				movieSearchList.add(movie);
+			}
+		}
+		return movieSearchList;
+	}
+	
+	public Movie create (Movie movie) {
+		if(!movieList.contains(movie)) {
+			movieList.add(movie);
+		}
+		return movie;
+	}
+	
+	public Movie update (Movie movie) {
+		for(Movie movieDetail : movieList) {
+			if(movieDetail.getMovieId() == movie.getMovieId()) {
+				movieDetail.setMovieTitle(movie.getMovieTitle());
+				movieDetail.setMovieYear(movie.getMovieYear());
+				movieDetail.setMovieLanguage(movie.getMovieLanguage());
+				movieDetail.setMovieRenter(movie.getMovieRenter());
+				return movieDetail;
+			}
+		}
+		throw new IllegalArgumentException("MovieId to be updated is not found");
+	}
+	
+	public Movie delete (int id) {
+		for (Iterator<Movie> iterateMovie = movieList.iterator(); iterateMovie.hasNext(); ) {
+			Movie movie = iterateMovie.next();
+			if(movie.getMovieId() == id) {
+				movieList.remove(movie);
+				return movie;
+			}
+		}
+		throw new IllegalArgumentException("MovieId to be deleted is not found");
+	}
+	
+	public boolean rentMovie (int movieId, String user) {
+		for(Movie movie : movieList) {
+			if(movie.getMovieId() == movieId) {
+				if(movie.getMovieRenter() == "NOT RENTED") {
+					movie.setMovieRenter(user);
+					return true;
+				} else {
+					throw new IllegalArgumentException("false, movie is already rented");
+				}
+			}
+		}
+		return false;
+	}
 }
